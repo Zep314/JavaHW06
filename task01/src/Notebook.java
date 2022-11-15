01a983f5618c50f34ea931aa8760ad4d6709ecbf
Dmitry Mordanov <mordanov314@gmail.com>
2022-11-16 00:28:11 +0300

    commit2

---
+++ task01/src/Notebook.java
@@ -0,0 +1,9 @@
+public class Notebook {
+    public static String name;
+    public static String brand;
+    public static String processor;
+    public static Integer vOzu;
+    public static Integer vHDD;
+    public static String os;
+    public static String Color;
+}
